88cb838c56b4ac72ccecdd6e39bf924457c29843
AdaN6 <a_disney_da@hotmail.com>
2023-04-17 11:10:09 +0200

    created a new route for EditProject

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AddProject from '../views/AddProject.vue'
+import EditProject from '../views/EditProject.vue'
 
 const routes = [
   {
@@ -13,6 +14,11 @@ const routes = [
     name: 'AddProject',
     component: AddProject
   },
+  {
+    path: '/projects/:id',
+    name: 'EditProject',
+    component: EditProject
+  }
   
 ]
 
