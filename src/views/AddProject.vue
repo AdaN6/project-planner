68fe4ec160a74d0d3d8b883b6554e7b9fba2e6d3
AdaN6 <a_disney_da@hotmail.com>
2023-04-17 10:03:19 +0200

    created a redirection after adding new project

--- src/views/AddProject.vue
+++ src/views/AddProject.vue
@@ -29,7 +29,10 @@ export default {
             headers: { 'Content-Type': 'application/json'},
             body: JSON.stringify(project)
            })
-           
+           .then(() => {
+            this.$router.push('/')
+           })
+           .catch(err => console.log(err.message))
            
         
         }
